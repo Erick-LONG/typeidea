22f7ec739b46c6f23ac78b7da4c2f60230fec38e
ericklong <834424581@qq.com>
2019-05-08 20:43:49 +0800

    增加搜索，作者，友链

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.conf.urls import url
 from .custom_site import custom_site
 
-from blog.views import (IndexView,CategoryView,TagView,PostDetailView)
-from config.views import links
+from blog.views import (IndexView,CategoryView,TagView,PostDetailView,SearchView,AuthorView)
+from config.views import LinkListView
 
 urlpatterns = [
     url(r'^super_admin/', admin.site.urls,name='super-admin'),
@@ -28,5 +28,7 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)$', CategoryView.as_view(),name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)$', TagView.as_view(),name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(),name='post-detail'),
-    url(r'^links/$', links,name='links')
+    url(r'^links/$', LinkListView.as_view(),name='links'),
+    url(r'^search/$', SearchView.as_view(),name='search'),
+    url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(),name='author'),
 ]
